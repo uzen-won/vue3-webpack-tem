ef4013bf87f024eedab6677186a21c13ac0dcf4d
uzen-won <daewon2197@gmail.com>
2023-07-26 14:19:06 +0900

    start project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    //vue
+    //"plugin:vue/vue3-essential",
+    "plugin:vue/vue3-strongly-recommend",
+    //"plugin:vue/vue3-recommend",
+    //js
+    "eslint:recommended",
+  ],
+  parserOptions: {
+    parser : 'babel-eslint'
+  },
+  rules: {
+    
+  },
+};
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1 @click="increase">{{ count }}</h1>
+  <div v-if="count > 4">4보다 큽니다!</div>
+  <ul>
+    <Fruit v-for="fruit in fruits" :key="fruit" :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>  
+
+<script>
+import Fruit from '~/components/Fruit'
+
+export default {
+  components: { //import 등록
+    //Fruit: Fruit   객체데이터 내부에서 속성이름과 데이터의 이름이 같으면 :뒤쪽 생략 가능
+    Fruit
+  },
+  data() {
+    return {
+      count: 0,
+      fruits: [
+        'Apple', 'Banana', 'Cherry'
+      ]
+    }
+  },
+  methods: {
+    increase() { //함수
+      this.count += 1 //this = script
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+h1 {
+  font-size: 50px;
+  color: royalblue;
+  cursor: pointer;
+}
+</style>
