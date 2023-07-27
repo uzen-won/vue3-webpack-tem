559dc2a88f56e2f4f73a4a0ff9f22d1849b2b12d
uzen-won <118948248+uzen-won@users.noreply.github.com>
2023-07-27 15:05:28 +0900

    Reset App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld :msg="message" @click="handler" />
-  <HelloWorld msg="Good" />
+  
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
 
-export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
-    }
-  }
-}
 </script>
